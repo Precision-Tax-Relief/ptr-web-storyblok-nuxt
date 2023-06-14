16f8edc33770a8890f41bfd753464ee7247b77b8
Logan-Ruf <cloganruf@gmail.com>
2023-06-14 13:23:32 -0700

    Updated gitignore and code styles

---
+++ .idea/fileTemplates/StoryBlok.vue
@@ -0,0 +1,17 @@
+<script setup lang='ts'>
+interface PropTypes {
+  blok: Object
+}
+const props = defineProps<PropTypes>()
+#[[$END$]]#
+</script>
+
+<template>
+  <div v-editable="props.blok">
+    <StoryblokComponent
+      v-for="child_block in blok.body"
+      :key="child_block._uid"
+      :blok="child_block"
+    />
+  </div>
+</template>
