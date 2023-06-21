57f632ca0852b00885dbd6f7f7e9d170003ae59d
Nic Trefts <nic.bassplayer@yahoo.com>
2023-06-21 16:40:10 -0700

    Get Gallery page working at desktop breakpoint. Fix Hero paragraph spacing.

---
+++ storyblok/GalleryPanel.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+interface PropTypes {
+  blok: blok
+}
+
+const imgDivStyles = [
+  "row-span-2 col-span-2",
+  "row-span-1 col-span-1",
+  "row-span-1 col-span-1",
+  "row-span-2 col-span-2",
+  "row-span-1 col-span-2"
+]
+
+const props = defineProps<PropTypes>()
+</script>
+
+<template>
+  <div v-editable="props.blok">
+    <div class="mx-auto grid gap-2 lg:auto-rows-[200px] lg:grid-cols-4">
+      <div v-for="(image, index) in props.blok?.images" :class="'bg-charcoal-200 ' + imgDivStyles[index % 5]">
+        <NuxtPicture
+          class="h-full w-full"
+          sizes=""
+          :src="image?.filename"
+          loading="lazy"
+          :imgAttrs="{ class: 'object-cover h-full w-full', alt: image?.alt }"
+        >
+        </NuxtPicture>
+      </div>
+    </div>
+  </div>
+</template>
