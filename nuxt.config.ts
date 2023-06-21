147c9528d9f2eb10615f42445fcb7eb98366d01b
Logan-Ruf <cloganruf@gmail.com>
2023-06-20 20:58:06 -0700

    Added route prefetching generation. This is for testing, needs to be make more stable for production.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import type { NuxtConfig } from "@nuxt/types"
 import { RichTextSchema } from "@storyblok/js"
+import { fetchStories } from "./utils/fetchStories"
 
 const config: NuxtConfig = {
   target: "static",
@@ -58,6 +59,38 @@ const config: NuxtConfig = {
     pageTransition: {
       name: "page",
       mode: "out-in"
+    }
+  },
+  hooks: {
+    async "nitro:config"(nitroConfig) {
+      if (!nitroConfig || nitroConfig.dev) {
+        return
+      }
+      const token = process.env.STORYBLOCK_TOKEN
+
+      let cache_version = 0
+
+      console.log(`https://api-us.storyblok.com/v2/cdn/spaces/me?token=${token}`)
+
+      // other routes that are not in Storyblok with their slug.
+      let routes = ["/"] // adds home directly but with / instead of /home
+      try {
+        const result = await fetch(`https://api-us.storyblok.com/v2/cdn/spaces/me?token=${token}`)
+
+        if (!result.ok) {
+          throw new Error("Could not fetch Storyblok data")
+        }
+        // timestamp of latest publish
+        const space = await result.json()
+        cache_version = space.space.version
+
+        // Recursively fetch all routes and set them to the routes array
+        await fetchStories(routes, cache_version)
+        // Adds the routes to the prerenderer
+        nitroConfig.prerender.routes.push(...routes)
+      } catch (error) {
+        console.error(error)
+      }
     }
   }
 }
---
+++ utils/fetchStories.ts
@@ -0,0 +1,34 @@
+const toIgnore = ["home", "layouts"]
+
+export async function fetchStories(routes: string[], cacheVersion: number, page: number = 1) {
+  const token = process.env.STORYBLOCK_TOKEN
+  const version = "published"
+  const perPage = 100
+
+  try {
+    const response = await fetch(
+      `https://api-us.storyblok.com/v2/cdn/links?token=${token}&version=${version}&per_page=${perPage}&page=${page}&cv=${cacheVersion}`
+    )
+    const data = await response.json()
+
+    console.log(JSON.stringify(data, null, 2))
+
+    // Add routes to the array
+    Object.values(data.links).forEach((link) => {
+      if (!toIgnore.includes(link.slug)) {
+        routes.push("/" + link.slug)
+      }
+    })
+
+    // Check if there are more pages with links
+
+    const total = response.headers.get("total")
+    const maxPage = Math.ceil(total / perPage)
+
+    if (maxPage > page) {
+      await fetchStories(routes, cacheVersion, ++page)
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
