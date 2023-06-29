2dc0bf98a0352401ec04f684f953b099773fcbd5
Logan-Ruf <cloganruf@gmail.com>
2023-06-28 22:56:10 -0700

    Add ColumnTwoDivider

---
+++ storyblok/ColumnTwoDivider.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { ColumnTwoDividerStoryblok } from "~/types/component-types-sb"
+
+interface PropTypes {
+  blok: ColumnTwoDividerStoryblok
+}
+
+const props = defineProps<PropTypes>()
+</script>
+
+<template>
+  <div v-editable="props.blok" class="mx-auto my-16 max-w-7xl px-6 sm:my-24 lg:px-8">
+    <h2 class="mb-12 text-center text-3xl font-bold text-charcoal-900 md:text-5xl" v-html="props.blok.header"></h2>
+    <div class="mx-auto grid max-w-2xl grid-cols-1 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+      <div class="flex flex-col pb-10 sm:pb-16 lg:pb-10 lg:pr-8 xl:pr-20">
+        <figure class="mt-10 flex flex-auto flex-col">
+          <h3 class="pb-2 text-3xl font-semibold text-charcoal-700 md:text-2xl">
+            {{ props.blok.headerOne }}
+          </h3>
+          <p class="text-lg leading-8 text-charcoal-700">
+            {{ props.blok.contentOne }}
+          </p>
+        </figure>
+      </div>
+      <div
+        class="flex flex-col border-t border-charcoal-100 pt-10 sm:pt-16 lg:border-l lg:border-t-0 lg:pb-10 lg:pl-8 lg:pt-0 xl:pl-20"
+      >
+        <figure class="mt-10 flex flex-auto flex-col">
+          <h3 class="pb-2 text-3xl font-semibold text-charcoal-700 md:text-2xl">
+            {{ props.blok.headerTwo }}
+          </h3>
+          <p class="text-lg leading-8 text-charcoal-700">
+            {{ props.blok.contentTwo }}
+          </p>
+        </figure>
+      </div>
+    </div>
+  </div>
+</template>
--- types/component-types-sb.d.ts
+++ types/component-types-sb.d.ts
@@ -1,292 +1,303 @@
-import { StoryblokStory } from "storyblok-generate-ts"
+import {StoryblokStory} from 'storyblok-generate-ts'
 
 export interface AllArticlesStoryblok {
-  title?: string
-  _uid: string
-  component: "AllArticles"
-  [k: string]: any
+  title?: string;
+  _uid: string;
+  component: "AllArticles";
+  [k: string]: any;
 }
 
 export interface AssetStoryblok {
-  alt?: string
-  copyright?: string
-  id: number
-  filename: string
-  name: string
-  title?: string
-  focus?: string
-  [k: string]: any
+  alt?: string;
+  copyright?: string;
+  id: number;
+  filename: string;
+  name: string;
+  title?: string;
+  focus?: string;
+  [k: string]: any;
 }
 
 export interface RichtextStoryblok {
-  type: string
-  content?: RichtextStoryblok[]
-  marks?: RichtextStoryblok[]
-  attrs?: any
-  text?: string
-  [k: string]: any
+  type: string;
+  content?: RichtextStoryblok[];
+  marks?: RichtextStoryblok[];
+  attrs?: any;
+  text?: string;
+  [k: string]: any;
 }
 
 export interface ArticleStoryblok {
-  image: AssetStoryblok
-  title: string
-  date?: string
-  teaser?: string
-  content?: RichtextStoryblok
-  _uid: string
-  component: "Article"
-  [k: string]: any
+  image: AssetStoryblok;
+  title: string;
+  date?: string;
+  teaser?: string;
+  content?: RichtextStoryblok;
+  _uid: string;
+  component: "Article";
+  [k: string]: any;
 }
 
 export type MultilinkStoryblok =
   | {
-      cached_url?: string
-      linktype?: string
-      [k: string]: any
+      cached_url?: string;
+      linktype?: string;
+      [k: string]: any;
     }
   | {
-      id?: string
-      cached_url?: string
-      anchor?: string
-      linktype?: "story"
+      id?: string;
+      cached_url?: string;
+      anchor?: string;
+      linktype?: "story";
       story?: {
-        name: string
-        created_at?: string
-        published_at?: string
-        id: number
-        uuid: string
+        name: string;
+        created_at?: string;
+        published_at?: string;
+        id: number;
+        uuid: string;
         content?: {
-          [k: string]: any
-        }
-        slug: string
-        full_slug: string
-        sort_by_date?: null | string
-        position?: number
-        tag_list?: string[]
-        is_startpage?: boolean
-        parent_id?: null | number
+          [k: string]: any;
+        };
+        slug: string;
+        full_slug: string;
+        sort_by_date?: null | string;
+        position?: number;
+        tag_list?: string[];
+        is_startpage?: boolean;
+        parent_id?: null | number;
         meta_data?: null | {
-          [k: string]: any
-        }
-        group_id?: string
-        first_published_at?: string
-        release_id?: null | number
-        lang?: string
-        path?: null | string
-        alternates?: any[]
-        default_full_slug?: null | string
-        translated_slugs?: null | any[]
-        [k: string]: any
-      }
-      [k: string]: any
+          [k: string]: any;
+        };
+        group_id?: string;
+        first_published_at?: string;
+        release_id?: null | number;
+        lang?: string;
+        path?: null | string;
+        alternates?: any[];
+        default_full_slug?: null | string;
+        translated_slugs?: null | any[];
+        [k: string]: any;
+      };
+      [k: string]: any;
     }
   | {
-      url?: string
-      cached_url?: string
-      anchor?: string
-      linktype?: "asset" | "url"
-      [k: string]: any
+      url?: string;
+      cached_url?: string;
+      anchor?: string;
+      linktype?: "asset" | "url";
+      [k: string]: any;
     }
   | {
-      email?: string
-      linktype?: "email"
-      [k: string]: any
-    }
+      email?: string;
+      linktype?: "email";
+      [k: string]: any;
+    };
 
 export interface ButtonOutlineStoryblok {
-  text?: string
-  link?: MultilinkStoryblok
-  _uid: string
-  component: "ButtonOutline"
-  [k: string]: any
+  text?: string;
+  link?: MultilinkStoryblok;
+  _uid: string;
+  component: "ButtonOutline";
+  [k: string]: any;
+}
+
+export interface ColumnTwoDividerStoryblok {
+  header?: string;
+  headerOne?: string;
+  contentOne?: string;
+  headerTwo?: string;
+  contentTwo?: string;
+  _uid: string;
+  component: "ColumnTwoDivider";
+  [k: string]: any;
 }
 
 export interface ContentCTAStoryblok {
-  title?: string
-  text?: string
-  ctas?: (PrimaryButtonStoryblok | SecondaryButtonStoryblok | ButtonOutlineStoryblok)[]
-  _uid: string
-  component: "ContentCTA"
-  [k: string]: any
+  title?: string;
+  text?: string;
+  ctas?: (PrimaryButtonStoryblok | SecondaryButtonStoryblok | ButtonOutlineStoryblok)[];
+  _uid: string;
+  component: "ContentCTA";
+  [k: string]: any;
 }
 
 export interface ContentFloaterStoryblok {
-  contentFloaterTitle?: string
-  contentFloaterBigText?: string
-  contentFloaterSmallText?: string
-  contentFloaterFigures?: any[]
-  _uid: string
-  component: "ContentFloater"
-  [k: string]: any
+  contentFloaterTitle?: string;
+  contentFloaterBigText?: string;
+  contentFloaterSmallText?: string;
+  contentFloaterFigures?: any[];
+  _uid: string;
+  component: "ContentFloater";
+  [k: string]: any;
 }
 
 export interface ContentFloaterFigureStoryblok {
-  value?: string
-  text?: string
-  _uid: string
-  component: "ContentFloaterFigure"
-  [k: string]: any
+  value?: string;
+  text?: string;
+  _uid: string;
+  component: "ContentFloaterFigure";
+  [k: string]: any;
 }
 
 export interface FeatureStoryblok {
-  name?: string
-  _uid: string
-  component: "feature"
-  [k: string]: any
+  name?: string;
+  _uid: string;
+  component: "feature";
+  [k: string]: any;
 }
 
 export interface FeaturesStoryblok {
-  _uid: string
-  component: "Features"
-  [k: string]: any
+  _uid: string;
+  component: "Features";
+  [k: string]: any;
 }
 
 export interface FooterBlokStoryblok {
-  links?: FooterLinkStoryblok[]
-  icons?: IconLinkStoryblok[]
-  copy?: string
-  _uid: string
-  component: "FooterBlok"
-  [k: string]: any
+  links?: FooterLinkStoryblok[];
+  icons?: IconLinkStoryblok[];
+  copy?: string;
+  _uid: string;
+  component: "FooterBlok";
+  [k: string]: any;
 }
 
 export interface FooterLinkStoryblok {
-  text?: string
-  link?: MultilinkStoryblok
-  _uid: string
-  component: "FooterLink"
-  [k: string]: any
+  text?: string;
+  link?: MultilinkStoryblok;
+  _uid: string;
+  component: "FooterLink";
+  [k: string]: any;
 }
 
 export interface GalleryStoryblok {
-  headingImage?: AssetStoryblok
-  panels?: GalleryPanelStoryblok[]
-  _uid: string
-  component: "Gallery"
-  [k: string]: any
+  headingImage?: AssetStoryblok;
+  panels?: GalleryPanelStoryblok[];
+  _uid: string;
+  component: "Gallery";
+  [k: string]: any;
 }
 
 export type MultiassetStoryblok = {
-  alt?: string
-  copyright?: string
-  id: number
-  filename: string
-  name: string
-  title?: string
-  [k: string]: any
-}[]
+  alt?: string;
+  copyright?: string;
+  id: number;
+  filename: string;
+  name: string;
+  title?: string;
+  [k: string]: any;
+}[];
 
 export interface GalleryCustomStoryblok {
-  images?: MultiassetStoryblok
-  _uid: string
-  component: "GalleryCustom"
-  [k: string]: any
+  images?: MultiassetStoryblok;
+  _uid: string;
+  component: "GalleryCustom";
+  [k: string]: any;
 }
 
 export interface GalleryPanelStoryblok {
-  heading?: string
-  images?: MultiassetStoryblok
-  _uid: string
-  component: "GalleryPanel"
-  [k: string]: any
+  heading?: string;
+  images?: MultiassetStoryblok;
+  _uid: string;
+  component: "GalleryPanel";
+  [k: string]: any;
 }
 
 export interface GalleryPreviewStoryblok {
-  title?: string
-  link?: MultilinkStoryblok
-  images?: MultiassetStoryblok
-  _uid: string
-  component: "GalleryPreview"
-  [k: string]: any
+  title?: string;
+  link?: MultilinkStoryblok;
+  images?: MultiassetStoryblok;
+  _uid: string;
+  component: "GalleryPreview";
+  [k: string]: any;
 }
 
 export interface GetInTouchStoryblok {
-  title?: string
-  description?: string
-  addressLineOne?: string
-  addressLineTwo?: string
-  mapsLink?: MultilinkStoryblok
-  telNumber?: string
-  telLink?: MultilinkStoryblok
-  email?: string
-  emailLink?: MultilinkStoryblok
-  form?: any[]
-  _uid: string
-  component: "GetInTouch"
-  [k: string]: any
+  title?: string;
+  description?: string;
+  addressLineOne?: string;
+  addressLineTwo?: string;
+  mapsLink?: MultilinkStoryblok;
+  telNumber?: string;
+  telLink?: MultilinkStoryblok;
+  email?: string;
+  emailLink?: MultilinkStoryblok;
+  form?: any[];
+  _uid: string;
+  component: "GetInTouch";
+  [k: string]: any;
 }
 
 export interface GridStoryblok {
-  columns?: any[]
-  _uid: string
-  component: "grid"
-  [k: string]: any
+  columns?: any[];
+  _uid: string;
+  component: "grid";
+  [k: string]: any;
 }
 
 export interface HeroStoryblok {
-  title: string
-  bigText: string
-  smallText?: string
-  image: AssetStoryblok
-  _uid: string
-  component: "Hero"
-  [k: string]: any
+  title: string;
+  bigText: string;
+  smallText?: string;
+  image: AssetStoryblok;
+  _uid: string;
+  component: "Hero";
+  [k: string]: any;
 }
 
 export interface HeroVideoStoryblok {
-  topButton?: any
-  topButtonText?: string
-  topButtonLink?: MultilinkStoryblok
-  video?: AssetStoryblok
-  bigText?: string
-  smallText?: string
-  ctas?: any[]
-  _uid: string
-  component: "HeroVideo"
-  [k: string]: any
+  topButton?: any;
+  topButtonText?: string;
+  topButtonLink?: MultilinkStoryblok;
+  video?: AssetStoryblok;
+  bigText?: string;
+  smallText?: string;
+  ctas?: any[];
+  _uid: string;
+  component: "HeroVideo";
+  [k: string]: any;
 }
 
 export interface HomeFeaturesStoryblok {
-  title?: string
-  stats?: StatDataStoryblok[]
-  panels?: HomeFeaturesPanelStoryblok[]
-  _uid: string
-  component: "HomeFeatures"
-  [k: string]: any
+  title?: string;
+  stats?: StatDataStoryblok[];
+  panels?: HomeFeaturesPanelStoryblok[];
+  _uid: string;
+  component: "HomeFeatures";
+  [k: string]: any;
 }
 
 export interface HomeFeaturesPanelStoryblok {
-  heading?: string
-  content?: RichtextStoryblok
-  images?: MultiassetStoryblok
-  _uid: string
-  component: "HomeFeaturesPanel"
-  [k: string]: any
+  heading?: string;
+  content?: RichtextStoryblok;
+  images?: MultiassetStoryblok;
+  _uid: string;
+  component: "HomeFeaturesPanel";
+  [k: string]: any;
 }
 
 export interface HomeModelStoryblok {
-  model?: string
-  series?: string
-  startingPrice?: string
-  featuredImage: AssetStoryblok
-  stats?: ModelStatStoryblok[]
-  body?: any[]
-  _uid: string
-  component: "HomeModel"
-  [k: string]: any
+  model?: string;
+  series?: string;
+  startingPrice?: string;
+  featuredImage: AssetStoryblok;
+  stats?: ModelStatStoryblok[];
+  body?: any[];
+  _uid: string;
+  component: "HomeModel";
+  [k: string]: any;
 }
 
 export interface HubSpotFormStoryblok {
-  region?: string
-  portalId?: string
-  formId?: string
-  _uid: string
-  component: "HubSpotForm"
-  [k: string]: any
+  region?: string;
+  portalId?: string;
+  formId?: string;
+  _uid: string;
+  component: "HubSpotForm";
+  [k: string]: any;
 }
 
 export interface IconLinkStoryblok {
-  alt: string
+  alt: string;
   icon:
     | ""
     | "simple-icons:facebook"
@@ -294,31 +305,32 @@ export interface IconLinkStoryblok {
     | "simple-icons:houzz"
     | "simple-icons:instagram"
     | "simple-icons:linkedin"
-  link: MultilinkStoryblok
-  size: "" | "small" | "medium" | "large"
-  _uid: string
-  component: "IconLink"
-  [k: string]: any
+    | "BuilderTrendLogo";
+  link: MultilinkStoryblok;
+  size: "" | "small" | "medium" | "large";
+  _uid: string;
+  component: "IconLink";
+  [k: string]: any;
 }
 
 export interface ImageCardStoryblok {
-  image?: AssetStoryblok
-  category?: string
-  link?: MultilinkStoryblok
-  preheading?: string
-  heading?: string
-  subheading?: string
-  _uid: string
-  component: "ImageCard"
-  [k: string]: any
+  image?: AssetStoryblok;
+  category?: string;
+  link?: MultilinkStoryblok;
+  preheading?: string;
+  heading?: string;
+  subheading?: string;
+  _uid: string;
+  component: "ImageCard";
+  [k: string]: any;
 }
 
 export interface LayoutStoryblok {
-  header?: any[]
-  footer?: any[]
-  _uid: string
-  component: "layout"
-  [k: string]: any
+  header?: any[];
+  footer?: any[];
+  _uid: string;
+  component: "layout";
+  [k: string]: any;
 }
 
 export interface ModelStatStoryblok {
@@ -329,174 +341,174 @@ export interface ModelStatStoryblok {
     | "ph:ruler-light"
     | "ph:stairs-light"
     | "ph:toilet-light"
-    | "ph:garage-light"
-  value?: string
-  label?: string
-  _uid: string
-  component: "ModelStat"
-  [k: string]: any
+    | "ph:garage-light";
+  value?: string;
+  label?: string;
+  _uid: string;
+  component: "ModelStat";
+  [k: string]: any;
 }
 
 export interface NavBarStoryblok {
-  Desktop?: any
-  desktopNavigation?: (NavBarLinkStoryblok | NavBarDropdownStoryblok)[]
-  desktopSocial?: IconLinkStoryblok[]
-  Mobile?: any
-  mobileNavigation?: (NavBarMobileDisclosureStoryblok | NavBarMobileLinkStoryblok)[]
-  mobileSocial?: IconLinkStoryblok[]
-  _uid: string
-  component: "NavBar"
-  [k: string]: any
+  Desktop?: any;
+  desktopNavigation?: (NavBarLinkStoryblok | NavBarDropdownStoryblok)[];
+  desktopSocial?: IconLinkStoryblok[];
+  Mobile?: any;
+  mobileNavigation?: (NavBarMobileDisclosureStoryblok | NavBarMobileLinkStoryblok)[];
+  mobileSocial?: IconLinkStoryblok[];
+  _uid: string;
+  component: "NavBar";
+  [k: string]: any;
 }
 
 export interface NavBarDropdownStoryblok {
-  text?: string
-  dropdown?: NavBarDropdownLinkStoryblok[]
-  _uid: string
-  component: "NavBarDropdown"
-  [k: string]: any
+  text?: string;
+  dropdown?: NavBarDropdownLinkStoryblok[];
+  _uid: string;
+  component: "NavBarDropdown";
+  [k: string]: any;
 }
 
 export interface NavBarDropdownLinkStoryblok {
-  text?: string
-  link?: MultilinkStoryblok
-  _uid: string
-  component: "NavBarDropdownLink"
-  [k: string]: any
+  text?: string;
+  link?: MultilinkStoryblok;
+  _uid: string;
+  component: "NavBarDropdownLink";
+  [k: string]: any;
 }
 
 export interface NavBarLinkStoryblok {
-  text?: string
-  link?: MultilinkStoryblok
-  _uid: string
-  component: "NavBarLink"
-  [k: string]: any
+  text?: string;
+  link?: MultilinkStoryblok;
+  _uid: string;
+  component: "NavBarLink";
+  [k: string]: any;
 }
 
 export interface NavBarMobileDisclosureStoryblok {
-  text?: string
-  panel?: NavBarMobileDisclosureLinkStoryblok[]
-  _uid: string
-  component: "NavBarMobileDisclosure"
-  [k: string]: any
+  text?: string;
+  panel?: NavBarMobileDisclosureLinkStoryblok[];
+  _uid: string;
+  component: "NavBarMobileDisclosure";
+  [k: string]: any;
 }
 
 export interface NavBarMobileDisclosureLinkStoryblok {
-  text?: string
-  link?: MultilinkStoryblok
-  _uid: string
-  component: "NavBarMobileDisclosureLink"
-  [k: string]: any
+  text?: string;
+  link?: MultilinkStoryblok;
+  _uid: string;
+  component: "NavBarMobileDisclosureLink";
+  [k: string]: any;
 }
 
 export interface NavBarMobileLinkStoryblok {
-  text?: string
-  link?: MultilinkStoryblok
-  _uid: string
-  component: "NavBarMobileLink"
-  [k: string]: any
+  text?: string;
+  link?: MultilinkStoryblok;
+  _uid: string;
+  component: "NavBarMobileLink";
+  [k: string]: any;
 }
 
 export interface PageStoryblok {
-  Meta?: any
-  title?: string
-  description?: string
-  image?: AssetStoryblok
-  body?: any[]
-  _uid: string
-  component: "page"
-  uuid?: string
-  [k: string]: any
+  Meta?: any;
+  title?: string;
+  description?: string;
+  image?: AssetStoryblok;
+  body?: any[];
+  _uid: string;
+  component: "page";
+  uuid?: string;
+  [k: string]: any;
 }
 
 export interface PopularArticlesStoryblok {
-  headline?: string
-  articles?: (StoryblokStory<ArticleStoryblok> | string)[]
-  _uid: string
-  component: "popular-articles"
-  [k: string]: any
+  headline?: string;
+  articles?: (StoryblokStory<ArticleStoryblok> | string)[];
+  _uid: string;
+  component: "popular-articles";
+  [k: string]: any;
 }
 
 export interface PrimaryButtonStoryblok {
-  text?: string
-  link?: MultilinkStoryblok
-  _uid: string
-  component: "PrimaryButton"
-  [k: string]: any
+  text?: string;
+  link?: MultilinkStoryblok;
+  _uid: string;
+  component: "PrimaryButton";
+  [k: string]: any;
 }
 
 export interface SecondaryButtonStoryblok {
-  text?: string
-  link?: MultilinkStoryblok
-  dark?: boolean
-  _uid: string
-  component: "SecondaryButton"
-  [k: string]: any
+  text?: string;
+  link?: MultilinkStoryblok;
+  dark?: boolean;
+  _uid: string;
+  component: "SecondaryButton";
+  [k: string]: any;
 }
 
 export interface SelectHomeModelStoryblok {
-  title?: string
-  homeModels?: (StoryblokStory<HomeModelStoryblok> | string)[]
-  _uid: string
-  component: "SelectHomeModel"
-  [k: string]: any
+  title?: string;
+  homeModels?: (StoryblokStory<HomeModelStoryblok> | string)[];
+  _uid: string;
+  component: "SelectHomeModel";
+  [k: string]: any;
 }
 
 export interface StatDataStoryblok {
-  name?: string
-  value?: string
-  _uid: string
-  component: "StatData"
-  [k: string]: any
+  name?: string;
+  value?: string;
+  _uid: string;
+  component: "StatData";
+  [k: string]: any;
 }
 
 export interface TabsVerticleFloaterStoryblok {
-  title?: string
-  subtitle?: string
-  tabs?: TabsVerticleFloaterTabStoryblok[]
-  _uid: string
-  component: "TabsVerticleFloater"
-  [k: string]: any
+  title?: string;
+  subtitle?: string;
+  tabs?: TabsVerticleFloaterTabStoryblok[];
+  _uid: string;
+  component: "TabsVerticleFloater";
+  [k: string]: any;
 }
 
 export interface TabsVerticleFloaterTabStoryblok {
-  title?: string
-  content?: string
-  logo?: AssetStoryblok
-  image?: AssetStoryblok
-  link?: MultilinkStoryblok
-  _uid: string
-  component: "TabsVerticleFloaterTab"
-  [k: string]: any
+  title?: string;
+  content?: string;
+  logo?: AssetStoryblok;
+  image?: AssetStoryblok;
+  link?: MultilinkStoryblok;
+  _uid: string;
+  component: "TabsVerticleFloaterTab";
+  [k: string]: any;
 }
 
 export interface TeaserStoryblok {
-  headline?: string
-  _uid: string
-  component: "teaser"
-  [k: string]: any
+  headline?: string;
+  _uid: string;
+  component: "teaser";
+  [k: string]: any;
 }
 
 export interface TestimonialStoryblok {
-  text?: string
-  name?: string
-  _uid: string
-  component: "Testimonial"
-  [k: string]: any
+  text?: string;
+  name?: string;
+  _uid: string;
+  component: "Testimonial";
+  [k: string]: any;
 }
 
 export interface TestimonialSliderStoryblok {
-  testimonials?: TestimonialStoryblok[]
-  _uid: string
-  component: "TestimonialSlider"
-  [k: string]: any
+  testimonials?: TestimonialStoryblok[];
+  _uid: string;
+  component: "TestimonialSlider";
+  [k: string]: any;
 }
 
 export interface ThreeColumnStoryblok {
-  title?: string
-  description?: string
-  content?: ImageCardStoryblok[]
-  _uid: string
-  component: "ThreeColumn"
-  [k: string]: any
+  title?: string;
+  description?: string;
+  content?: ImageCardStoryblok[];
+  _uid: string;
+  component: "ThreeColumn";
+  [k: string]: any;
 }
