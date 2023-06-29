3e5f5d9bc17c2d47e1be471a10851fdd3745c2c5
Logan-Ruf <cloganruf@gmail.com>
2023-06-28 17:31:56 -0700

    Finished styling for SelectHomeModel

--- components/HomeModelCard.vue
+++ components/HomeModelCard.vue
@@ -10,16 +10,19 @@ console.log()
 </script>
 
 <template>
-  <NuxtLink :to="'/' + props.slug">
+  <NuxtLink :to="'/' + props.slug" class="border border-charcoal-100 shadow-none hover:shadow-md shadow-charcoal-500/25 hover:shadow-charcoal-500/25 transition-shadow duration-300">
     <div>
       <NuxtPicture
         :src="props.model.featuredImage.filename"
-        :imgAttrs="{ alt: props.model.featuredImage.alt }"
+        :imgAttrs="{
+          alt: props.model.featuredImage.alt,
+          class: 'aspect-video object-cover'
+          }"
       ></NuxtPicture>
-      <h2 class="text-3xl font-bold text-charcoal-700">
+      <h2 class="text-3xl font-bold text-charcoal-700 tracking-tight p-2">
         {{ props.model.model }}
       </h2>
-      <div class="grid grid-cols-2 justify-between">
+      <div class="grid grid-cols-2 justify-between gap-2 px-2 pb-4">
         <div v-for="stat in props.model.stats.filter((stat) => stat.label != 'Floor')" class="space-x-2">
           <Icon :name="stat.icon" class="text-charcoal-500" />
           <span> {{ stat.value }} {{ stat.label }} </span>
--- storyblok/SelectHomeModel.vue
+++ storyblok/SelectHomeModel.vue
@@ -2,29 +2,31 @@
 import { useStoryblokApi } from "@storyblok/vue"
 import { SelectHomeModelStoryblok } from "~/types/component-types-sb"
 import HomeModelCard from "~/components/HomeModelCard.vue"
+
 interface PropTypes {
   blok: SelectHomeModelStoryblok
 }
+
 const props = defineProps<PropTypes>()
 
 const storyblokApi = useStoryblokApi()
 const { data } = await storyblokApi.get("cdn/stories", {
-  version: "draft",
+  version: "published",
   starts_with: "model",
-  by_uuids: props.blok.homeModels?.join(",")
+  by_uuids: props.blok.homeModels?.join(","),
+  resolve_links: "link"
 })
 const models = data.stories
 </script>
 
 <template>
-  <div v-editable="props.blok">
-    <div class="mx-auto">
-      <h2 class="text-center text-3xl font-bold tracking-tight text-charcoal-900 sm:text-4xl">
-        {{ props.blok.title }}
-      </h2>
-      <div v-for="model in models" class="grid grid-cols-3 gap-4">
-        <HomeModelCard :model="model.content"></HomeModelCard>
-      </div>
+  <div v-editable="props.blok" class="mx-auto container px-6 pt-6 pb-24">
+    <h2 class="text-center text-34xl font-bold tracking-tight text-charcoal-900 sm:text-4xl pb-12">
+
+      {{ props.blok.title }}
+    </h2>
+    <div class="grid grid-cols-1 sm:grid-cols-2 max-w-lg sm:max-w-max md:grid-cols-3 mx-auto gap-5">
+      <HomeModelCard v-for="model in models" :key="model.uid" :model="model.content" :slug="model.full_slug"></HomeModelCard>
     </div>
   </div>
 </template>
