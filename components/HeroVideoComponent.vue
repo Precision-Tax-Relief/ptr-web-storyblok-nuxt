eb4fee7828fe004b3f338fbf6a5f9e29c63d57b5
Logan-Ruf <cloganruf@gmail.com>
2023-08-06 14:15:22 -0700

    HeroVideo Redesign

--- components/HeroVideoComponent.vue
+++ components/HeroVideoComponent.vue
@@ -15,8 +15,8 @@ const props = defineProps<PropTypes>()
 </script>
 
 <template>
-  <div class="relative isolate overflow-hidden bg-neutral-900 pt-14">
-    <video autoplay loop playsinline muted class="absolute inset-0 z-0 h-full w-full object-cover brightness-50">
+  <div class="relative isolate flex min-h-screen flex-col overflow-hidden bg-neutral-900 pt-14 lg:flex-col-reverse">
+    <video autoplay loop playsinline muted class="absolute inset-0 z-0 h-full w-full object-cover brightness-75">
       <source :src="props.videoSrc + videoDelay" />
     </video>
     <div class="absolute inset-x-0 -top-40 z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
@@ -45,25 +45,27 @@ const props = defineProps<PropTypes>()
       />
     </div>
 
-    <div class="relative z-20 mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-      <div class="hidden sm:mb-8 sm:flex sm:justify-center">
-        <div
-          class="relative rounded-full px-3 py-1 text-sm leading-6 text-white ring-1 ring-white/10 hover:ring-white/50"
-        >
-          <slot name="top-button"></slot>
-          <NuxtLink :href="props.topLink" class="font-semibold text-white">
-            <span class="absolute inset-0" aria-hidden="true" /> Read more <span aria-hidden="true">&rarr;</span>
-          </NuxtLink>
-        </div>
-      </div>
-      <div class="text-center">
+    <div
+      class="relative z-20 m-12 max-w-3xl rounded-xl bg-gradient-to-r from-charcoal-600/90 to-charcoal-600/40 p-12 sm:my-24 sm:ml-16 lg:my-40 lg:ml-28"
+    >
+      <!--      <div class="hidden sm:mb-8 sm:flex sm:justify-center">-->
+      <!--        <div-->
+      <!--          class="relative rounded-full px-3 py-1 text-sm leading-6 text-white ring-1 ring-white/10 hover:ring-white/50"-->
+      <!--        >-->
+      <!--          <slot name="top-button"></slot>-->
+      <!--          <NuxtLink :href="props.topLink" class="font-semibold text-white">-->
+      <!--            <span class="absolute inset-0" aria-hidden="true" /> Read more <span aria-hidden="true">&rarr;</span>-->
+      <!--          </NuxtLink>-->
+      <!--        </div>-->
+      <!--      </div>-->
+      <div class="">
         <h1 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">
           <slot name="big-text"></slot>
         </h1>
-        <p class="mt-6 text-lg leading-8 text-charcoal-100">
+        <p class="mt-6 text-xl leading-8 text-charcoal-50">
           <slot name="small-text"></slot>
         </p>
-        <div class="mt-10 flex items-center justify-center gap-x-6">
+        <div class="mt-10 flex gap-x-6">
           <slot name="ctas"></slot>
         </div>
       </div>
