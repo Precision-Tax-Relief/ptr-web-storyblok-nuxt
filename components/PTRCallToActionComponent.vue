f861b4de4c4ab6b92887ef99b13b209fa40a4aa1
Logan Ruf <lruf@precisiontax.com>
2025-03-03 13:54:17 -0800

    Redid CSS on PTR CTA

--- components/PTRCallToActionComponent.vue
+++ components/PTRCallToActionComponent.vue
@@ -5,27 +5,27 @@
     <div class="section-head">
       <h2
         id="head_b_padding"
-        class="mx-auto mb-12 max-w-5xl text-center text-4xl font-bold leading-tight text-primary lg:mb-20 lg:text-[4.1rem]"
+        class="mb-12 px-24 text-center text-4xl font-bold leading-tight text-primary md:text-5xl lg:mb-20 lg:text-6xl"
       >
         What to expect during your free&nbsp;consultation
       </h2>
     </div>
-    <div class="lg:gap-15 my-20 flex flex-col justify-between gap-20 lg:flex-row">
+    <div class="my-10 flex flex-col justify-between gap-12 lg:flex-row">
       <div
         class="basis-1/3 rounded border border-[#e9e9e9] px-20 py-16 shadow-sm lg:rounded-none lg:border-0 lg:px-0 lg:py-0 lg:shadow-none"
       >
         <div class="relative flex items-center justify-between">
           <span
-            class="absolute -top-[60px] left-1/2 flex h-16 w-16 -translate-x-1/2 items-center justify-center rounded-full bg-blue-400 text-center text-4xl font-bold text-white lg:relative lg:left-0 lg:top-0 lg:translate-x-0 lg:text-4xl"
+            class="absolute -top-[60px] left-1/2 flex h-14 w-14 -translate-x-1/2 items-center justify-center rounded-full bg-blue-400 text-center text-3xl font-bold text-white lg:relative lg:left-0 lg:top-0 lg:translate-x-0"
             >1</span
           >
           <PTRArrow class="grow pl-4" />
         </div>
-        <h4 class="mb-8 text-[2.1rem] font-bold leading-[1.3] text-primary lg:my-8 xl:text-[2.7rem]">
+        <h4 class="mb-8 text-3xl leading-10 font-bold text-primary mt-3">
           Speak to a licensed <br />
           tax expert
         </h4>
-        <p class="text-2xl leading-normal xl:text-3xl xl:leading-[1.6]">
+        <p class="text-2xl leading-normal text-stone-800">
           Share details about your situation in a <strong>free, confidential consultation,</strong> and ask our tax
           professional any questions you have.
         </p>
@@ -35,15 +35,15 @@
       >
         <div class="flex flex-row items-center justify-between">
           <span
-            class="absolute -top-[60px] left-1/2 flex h-16 w-16 -translate-x-1/2 items-center justify-center rounded-full bg-blue-400 text-center text-4xl font-bold text-white lg:relative lg:left-0 lg:top-0 lg:translate-x-0 lg:text-4xl"
+            class="absolute -top-[60px] left-1/2 flex h-14 w-14 -translate-x-1/2 items-center justify-center rounded-full bg-blue-400 text-center text-3xl font-bold text-white lg:relative lg:left-0 lg:top-0 lg:translate-x-0"
             >2</span
           >
           <PTRArrow class="grow pl-4" />
         </div>
-        <h4 class="mb-8 text-[2.1rem] font-bold leading-[1.3] text-primary lg:my-8 xl:text-[2.7rem]">
-          Get an honest assessment of your situation
+        <h4 class="mb-8 text-3xl leading-10 font-bold text-primary mt-3">
+          Get an honest assessment <br />of your situation
         </h4>
-        <p class="text-2xl leading-normal xl:text-3xl xl:leading-[1.6]">
+        <p class="text-2xl leading-normal text-stone-800">
           Our tax professionals will assess your <strong>options for reaching a favorable solution</strong> to your tax
           problem. We may even be able to <strong>help you resolve your issue for free.</strong>
         </p>
@@ -53,16 +53,16 @@
       >
         <div class="relative flex items-center justify-between">
           <span
-            class="absolute -top-[60px] left-1/2 flex h-16 w-16 -translate-x-1/2 items-center justify-center rounded-full bg-blue-400 text-center text-4xl font-bold text-white lg:relative lg:left-0 lg:top-0 lg:translate-x-0 lg:text-4xl"
+            class="absolute -top-[60px] left-1/2 flex h-14 w-14 -translate-x-1/2 items-center justify-center rounded-full bg-blue-400 text-center text-3xl font-bold text-white lg:relative lg:left-0 lg:top-0 lg:translate-x-0"
             >3</span
           >
           <PTRArrow class="grow px-4" />
           <Icon name="material-symbols-light:check-circle" class="-my-4 -ml-4 -mr-1 text-green-500" size="69px" />
         </div>
-        <h4 class="mb-8 text-[2.1rem] font-bold leading-[1.3] text-primary lg:my-8 xl:text-[2.7rem]">
-          Get a transparent quote with affordable payments
+        <h4 class="mb-8 text-3xl leading-10 font-bold text-primary mt-3">
+          Get a transparent quote <br />with affordable payments
         </h4>
-        <p class="text-2xl leading-normal xl:text-3xl xl:leading-[1.6]">
+        <p class="text-2xl leading-normal text-stone-800">
           If your situation requires professional assistance, we'll provide you a transparent quote for our services
           with <strong>flexible payment options based on what you can afford.</strong> We NEVER request full payment up
           front.
@@ -73,7 +73,7 @@
       <a
         href="#page"
         data-click-name="ClickBody - Form Scroll  Below What To Expect"
-        class="mb-4 flex items-center justify-center gap-3 rounded-md bg-green-500 px-8 py-4 hover:bg-green-600 lg:px-16 lg:py-8"
+        class="mb-4 flex items-center justify-center gap-2 rounded-md bg-green-500 px-6 py-3 hover:bg-green-600 lg:px-10 lg:py-6"
       >
         <!--        <i class="fa-solid fa-phone text-[3rem] text-white lg:text-[4rem]"></i>-->
         <Icon name="fa-solid:phone-alt" class="text-5xl text-white lg:text-6xl" />
