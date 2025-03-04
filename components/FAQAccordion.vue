4f4283afca5a206999c6eaaf6f1eaa536c8ba920
Logan Ruf <lruf@precisiontax.com>
2025-03-04 08:37:45 -0800

    Added animation for hover and +/x for FAQ

--- components/FAQAccordion.vue
+++ components/FAQAccordion.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { FaqItemStoryblok } from "~/types/component-types-sb"
+
 interface PropTypes {
   title: string
   subtitle?: string
@@ -29,30 +30,20 @@ defineProps<PropTypes>()
         :key="index"
         v-slot="{ open }"
         as="article"
-        class="accordion__item js-show-item-default bg-white rounded-xl mx-4 lg:mx-0 mb-4"
+        class="bg-white mx-4 lg:mx-0 mb-4 overflow-hidden"
       >
         <div class="content">
           <HeadlessDisclosureButton
-            class="relative flex gap-8 items-center p-2 w-full"
+            class="relative rounded-xl flex gap-8 items-center p-2 w-full transition-colors duration-300 hover:bg-neutralLight"
             :aria-controls="`panel-${index}`"
             :aria-expanded="open"
           >
-            <span class="">
-              <span v-if="!open" class="flex items-center justify-center icon-faq-add">
-                <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <circle cx="22" cy="22" r="22" fill="#173F85" />
-                  <path
-                    d="M30.75 23.25H23.25V30.75H20.75V23.25H13.25V20.75H20.75V13.25H23.25V20.75H30.75V23.25Z"
-                    fill="white"
-                  />
-                </svg>
-              </span>
-              <span v-else class="flex items-center justify-center icon-faq-subtract">
-                <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <circle cx="22" cy="22" r="22" fill="#173F85" />
-                  <path d="M30.75 23.25H13.25V20.75H30.75V23.25Z" fill="white" />
-                </svg>
-              </span>
+            <span class="flex items-center justify-center icon-faq">
+              <Icon
+                name="mdi:add-circle"
+                class="transform transition-transform duration-300 ease-in-out w-12 h-12 text-primaryDark"
+                :class="{ 'rotate-45': open }"
+              />
             </span>
             <h3 :id="`tab-${index}`" class="font-bold text-2xl leading-normal text-primary text-left">
               {{ item.question }}
@@ -61,11 +52,12 @@ defineProps<PropTypes>()
 
           <HeadlessDisclosurePanel
             :id="`panel-${index}`"
-            class="accordion__content md:pl-[5rem] pl-5 pr-6 leading-[24px] text-[#333]"
+            class="md:pl-[5rem] pl-5 pr-6 leading-[24px] text-[#333] overflow-hidden transition-all duration-300 ease-out origin-top"
+            :class="open ? 'max-h-[1000px] opacity-100' : 'max-h-0 opacity-0'"
             role="tabpanel"
             :aria-labelledby="`tab-${index}`"
           >
-            <div class="accordion__content-inner pb-4 text-[1.5rem] md:text-md" v-html="item.answer" />
+            <div class="py-4 text-[1.5rem] md:text-md" v-html="item.answer" />
           </HeadlessDisclosurePanel>
         </div>
       </HeadlessDisclosure>
