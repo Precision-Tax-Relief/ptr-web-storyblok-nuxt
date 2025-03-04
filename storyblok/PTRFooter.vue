fe9e06e8530a373724ac540e8290c35c7d3c2f15
Logan Ruf <lruf@precisiontax.com>
2025-03-04 10:49:49 -0800

    Finished first pass of styling

--- storyblok/PTRFooter.vue
+++ storyblok/PTRFooter.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import type { PTRFooterStoryblok } from "~/types/component-types-sb"
+import type { PtrFooterStoryblok } from "~/types/component-types-sb"
 
 interface PropTypes {
-  blok: PTRFooterStoryblok
+  blok: PtrFooterStoryblok
 }
 
 const props = defineProps<PropTypes>()
@@ -25,115 +25,93 @@ const props = defineProps<PropTypes>()
                   class: 'mx-auto'
                 }"
               />
-              <figcaption class="text-[#96a7ba] pt-6 text-2xl">
+              <figcaption class="text-slate-300 text-lg font-light">
                 Our team consists of proven tax professionals that include CPAs, Tax Attorneys, and federally licensed
                 Enrolled Agents.
               </figcaption>
             </figure>
           </div>
           <aside class="text-center text-white my-8">
-            <h4 style="font-size: 3rem" class="font-bold">If you have a tax problem, the time to act is now.</h4>
-            <p class="text-white font-normal" style="font-size: 2.3rem; margin: 15px 0">
+            <h4 class="font-bold text-4xl pb-3">If you have a tax problem, the time to act is now.</h4>
+            <p class="text-white text-xl pb-4">
               Trust the experts at Precision Tax Relief to put it behind you for good.
             </p>
             <a
-              class="flex-col mx-auto bg-green-500 hover:bg-green-600 py-4 px-10 hidden sm:inline-flex items-center justify-center gap-1 my-16 select-none rounded-sm lg:rounded-md"
+              class="flex-col mx-auto bg-green-500 hover:bg-green-600 py-3 px-8 hidden sm:inline-flex items-center justify-center gap-1 select-none rounded-sm lg:rounded-md"
               href="#"
             >
-              <span class="text-[2.1rem]">Call us today:</span><strong class="text-6xl">1-844-229-6966</strong>
+              <span class="text-xl">Call us today:</span><strong class="text-4xl">1-844-229-6966</strong>
               <span>
-                <span
-                  class="underline text-3xl activate-form scrollTo"
-                  href="#"
-                  data-click-name="ClickFtr - Form Scroll"
+                <span class="underline text-2xl" href="#" data-click-name="ClickFtr - Form Scroll"
                   >or fill out the form above</span
                 >
               </span>
             </a>
           </aside>
         </div>
-        <div class="pb-20 flex justify-center justify-between items-center gap-20">
-          <span class="text-[#96a7ba] uppercase">proud members of:</span>
-          <ul class="flex gap-20">
-            <li>
-              <div class="icon_aicpa">
-                <picture
-                  class="lozad"
-                  data-iesrc="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/aicpa.png"
-                  data-loaded="true"
-                  ><source
-                    srcset="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/aicpa.webp"
-                    type="image/webp"
-                  >
-                  <source
-                    srcset="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/aicpa.png"
-                    type="image/png"
-                  >
-                  <img
-                    src="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/aicpa.png"
-                    alt="aicpa logo"
-                  >
-                </picture>
-              </div>
-            </li>
-            <li>
-              <div class="icon_naea">
-                <picture
-                  class="lozad"
-                  data-iesrc="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/naea.png"
-                  data-loaded="true"
-                  ><source
-                    srcset="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/naea.webp"
-                    type="image/webp"
-                  >
-                  <source
-                    srcset="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/naea.png"
-                    type="image/png"
-                  >
-                  <img src="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/naea.png" alt="naea" >
-                </picture>
-              </div>
-            </li>
-            <li>
-              <div class="icon_astps">
-                <picture
-                  class="lozad"
-                  data-iesrc="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/astps.png"
-                  data-loaded="true"
-                  ><source
-                    srcset="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/astps.webp"
-                    type="image/webp"
-                  >
-                  <source
-                    srcset="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/astps.png"
-                    type="image/png"
-                  >
-                  <img src="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/astps.png" alt="astps" >
-                </picture>
-              </div>
-            </li>
-          </ul>
+        <div class="py-16 flex justify-between items-center gap-20">
+          <span class="text-slate-300 uppercase">Proud members of:</span>
+          <picture
+            class="lozad"
+            data-iesrc="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/aicpa.png"
+            data-loaded="true"
+            ><source
+              srcset="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/aicpa.webp"
+              type="image/webp"
+            />
+            <source
+              srcset="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/aicpa.png"
+              type="image/png"
+            />
+            <img src="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/aicpa.png" alt="aicpa logo" />
+          </picture>
+          <picture
+            class="lozad"
+            data-iesrc="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/naea.png"
+            data-loaded="true"
+            ><source
+              srcset="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/naea.webp"
+              type="image/webp"
+            />
+            <source
+              srcset="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/naea.png"
+              type="image/png"
+            />
+            <img src="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/naea.png" alt="naea" />
+          </picture>
+          <picture
+            class="lozad"
+            data-iesrc="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/astps.png"
+            data-loaded="true"
+            ><source
+              srcset="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/astps.webp"
+              type="image/webp"
+            />
+            <source
+              srcset="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/astps.png"
+              type="image/png"
+            />
+            <img src="//prod-taxattorney-bbb.azureedge.net/assets/images/shared/logos/astps.png" alt="astps" />
+          </picture>
         </div>
-        <div class="text-center text-[#65707f] leading-[24px] text-[1.4rem] mb-2">
+        <div class="text-center text-slate-400 text-sm mb-1">
           <p>
             We will not rent, share, or sell your information I <a id="open-privacy-modal" href="#">Privacy Policy</a>
           </p>
           <p>Contact us: Precision Tax Relief 1-844-229-6966 I Address: 233 E Locust Ave, Coeur d' Alene, ID 83814</p>
-        </div>
-        <div class="text-center text-[#65707f] leading-[24px] text-[1.4rem]">
           <p>Copyright 2025 Precision Tax Relief, LLC | All Rights Reserved</p>
         </div>
-        <div id="disclaimers" class="text-white text-[1.1rem] py-10">
-          <p class="mb-5">
+        <div id="disclaimers" class="text-slate-300 gap-2 flex flex-col text-xs py-4">
+          <p>
             [1] Precision Tax Relief is a registered trademark of Precision Tax Relief, LLC. All other trademarks, trade
             names, product names, logos, and brands are property of their respective owners. Use of these trademarks,
             trade names, product names, logos, and brands does not imply endorsement.
           </p>
-          <p class="mb-5">
+          <p>
             [2] Statistics are based on prior results for clients. Estimates are based on prior results; individual
             results will vary based on circumstances.
           </p>
-          <p class="mb-5">
+          <p>
             [3] The company data presented in the competitor chart on our website is updated on a monthly basis. This
             data is sourced directly from BBB.org, Google.com, and Trustpilot.com,. While we strive to ensure the
             accuracy and completeness of the information, we cannot guarantee that it is always up-to-date or
