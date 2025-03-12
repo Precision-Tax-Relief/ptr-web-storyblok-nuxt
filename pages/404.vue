29debd9350e45d7ec985abc40117af06de93c9e0
Logan Ruf <lruf@precisiontax.com>
2025-03-12 16:51:53 -0700

    Added 404

---
+++ pages/404.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container mx-auto px-4 py-32">
+    <div class="max-w-4xl mx-auto">
+      <h1 class="text-center text-4xl md:text-5xl font-bold text-charcoal-800 mb-6">404 - Page Not Found</h1>
+    </div>
+  </div>
+</template>
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,4 +1,5 @@
 <script setup>
+const router = useRouter()
 useHead({
   htmlAttrs: {
     lang: "en"
@@ -11,8 +12,13 @@ const { slug } = useRoute().params
 const story = await useAsyncStoryblok(slug && slug.length > 0 ? slug.join("/").replace(/\/$/, "") : "index", {
   version: config.public.storyblokVersion
 })
+console.log("story", !!story?.content)
+if (!story?.content) {
+  console.log("go to 404")
+  router.push({ path: "/404" })
+}
 </script>
 
 <template>
-  <StoryblokComponent v-if="story" :blok="story.content" />
+  <StoryblokComponent v-if="story?.content" :blok="story.content" />
 </template>
