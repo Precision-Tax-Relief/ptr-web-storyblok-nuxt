408608bbe6948ece2b9a85faf34fc37e6fdb4cb8
Fucci <benjaminakacha@gmail.com>
2025-06-30 16:49:35 -0700

    fix: added required fixes to background shadow to mimic exit intent popup

--- components/ContactFormPopup.vue
+++ components/ContactFormPopup.vue
@@ -34,13 +34,17 @@ onUnmounted(() => {
       </slot>
     </div>
 
-    <div v-if="showPopup" class="fixed inset-0 z-50 flex items-center justify-center p-0" @click="closePopup">
+    <div
+      v-if="showPopup"
+      class="fixed inset-0 z-50 flex items-center justify-center bg-gray-500/75 p-0 shadow-xl transition-opacity"
+      @click="closePopup"
+    >
       <div class="w-md relative max-w-md" @click.stop>
         <!-- Close Button -->
         <div class="absolute -top-10 right-0 z-50">
           <button
             type="button"
-            class="flex rounded-md bg-white text-gray-400 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-primaryDark focus:ring-offset-2"
+            class="flex rounded-md bg-white hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-primaryDark focus:ring-offset-2"
             @click="closePopup"
           >
             <span class="sr-only">Close</span>
