b8cede435528b89fbcfc2e0b1c5689ffe4c807b3
Fucci <benjaminakacha@gmail.com>
2025-06-30 16:52:58 -0700

    fix: removed useless divs with min-h and min-2

--- storyblok/PTRHero.vue
+++ storyblok/PTRHero.vue
@@ -81,18 +81,16 @@ const props = defineProps<PropTypes>()
                 to="https://www.precisiontaxrelief.com/pdf/TorchAwards_Certificate_Recipient_2023.pdf"
                 label="Torch Awards 2023 PDF"
               >
-                <div class="relative h-[90px] w-[131px]">
-                  <NuxtImg
-                    src="https://a-us.storyblok.com/f/1023258/131x90/5b311ae5d1/tafe2023.png"
-                    alt="BBB Torch Awards for Ethics 2023 Winner"
-                    format="webp"
-                    loading="eager"
-                    width="131"
-                    height="90"
-                    fetchpriority="high"
-                    sizes="131 "
-                  ></NuxtImg>
-                </div>
+                <NuxtImg
+                  src="https://a-us.storyblok.com/f/1023258/131x90/5b311ae5d1/tafe2023.png"
+                  alt="BBB Torch Awards for Ethics 2023 Winner"
+                  format="webp"
+                  loading="eager"
+                  width="131"
+                  height="90"
+                  fetchpriority="high"
+                  sizes="131 "
+                ></NuxtImg>
               </SegmentExternalLink>
             </div>
           </li>
@@ -103,23 +101,21 @@ const props = defineProps<PropTypes>()
                 to="https://www.precisiontaxrelief.com/pdf/TorchAwards_Certificate_Recipient_2019.pdf"
                 label="Torch Awards 2019 PDF"
               >
-                <div class="relative h-[90px] min-h-[90px] w-[131px] min-w-[131px]">
-                  <NuxtImg
-                    src="https://a-us.storyblok.com/f/1023258/134x90/35be32afe6/tafe2019.png"
-                    alt="BBB Torch Awards for Ethics 2019 Winner"
-                    format="webp"
-                    loading="eager"
-                    width="131"
-                    height="90"
-                    fetchpriority="high"
-                    sizes="131"
-                  />
-                </div>
+                <NuxtImg
+                  src="https://a-us.storyblok.com/f/1023258/134x90/35be32afe6/tafe2019.png"
+                  alt="BBB Torch Awards for Ethics 2019 Winner"
+                  format="webp"
+                  loading="eager"
+                  width="131"
+                  height="90"
+                  fetchpriority="high"
+                  sizes="131"
+                />
               </SegmentExternalLink>
             </div>
           </li>
           <li class="mt-4 flex flex-col items-center lg:mt-0">
-            <div class="badge badge_google relative h-[50px] min-h-[50px]">
+            <div class="badge badge_google">
               <NuxtImg
                 src="https://a-us.storyblok.com/f/1023258/479x120/ab7cca33fc/bbb-mobile.png"
                 alt="BBB A+ Accredited Business"
